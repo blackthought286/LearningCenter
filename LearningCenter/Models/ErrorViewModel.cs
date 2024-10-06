2df98f3689c4aeb39e643a63c0acd1d88ff31acb
Everett <blackthought286@gmail.com>
2024-10-06 08:06:14 -0400

    initial commit

---
+++ LearningCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LearningCenter.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
